6477a467759f4f4678693af598b1f614de83f272
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-18 15:08:02 +0700

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -33,9 +33,10 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001680367997
+                                #and chat_id != -1001190342892
+                                #and chat_id != -1001733534088
+                                #and chat_id != -1001443281821
                             ):
                                 if not await is_active_chat(chat_id):
                                     try:
