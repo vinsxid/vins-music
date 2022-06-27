00adebbbb7045b957855567c93094fbbadad81ec
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-27 16:26:26 +0700

    Update mongo.py

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -17,7 +17,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://vinsmusic:vinsmusic@cluster0.bqstz.mongodb.net/?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
