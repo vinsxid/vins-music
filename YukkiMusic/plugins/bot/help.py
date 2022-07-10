a7c3c985f010968742835c300a80a002e7c3caa3
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-07-11 06:21:29 +0700

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -95,7 +95,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb5":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "Only for Sudo Users", show_alert=True
+                "🧑‍🔧 Hanya Untuk Admin Sudo", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
