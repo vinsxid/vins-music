779abe2050be8970ac606603e9f2ea241b142d03
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-07-15 19:35:35 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -36,19 +36,19 @@ def stream_markup(_, videoid):
     buttons = [
         [
             InlineKeyboardButton(
-            #    text=_["PL_B_2"],
-            #    callback_data=f"add_playlist {videoid}",
-                text=_["PL_B_3"], switch_inline_query_current_chat=""
+                text=_["PL_B_2"],
+                callback_data=f"add_playlist {videoid}",
+                #text=_["PL_B_3"], switch_inline_query_current_chat=""
             ),
             InlineKeyboardButton(
-               #text=_["PL_B_3"], switch_inline_query_current_chat=""
-               text=_["CLOSEMENU_BUTTON"], callback_data="close"
+               text=_["PL_B_3"], switch_inline_query_current_chat=""
+               #text=_["CLOSEMENU_BUTTON"], callback_data="close"
             ),
         ],
         [
-           # InlineKeyboardButton(
-           #     text=_["CLOSEMENU_BUTTON"], callback_data="close"
-           # )
+            InlineKeyboardButton(
+                text=_["CLOSEMENU_BUTTON"], callback_data="close"
+            )
         ],
     ]
     return buttons
