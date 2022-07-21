2e52f77e90d0268509ba0682777e7828638d291b
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-07-21 10:03:20 +0700

    Update sudoers.py

--- YukkiMusic/plugins/sudo/sudoers.py
+++ YukkiMusic/plugins/sudo/sudoers.py
@@ -30,7 +30,7 @@ SUDOUSERS_COMMAND = get_command("SUDOUSERS_COMMAND")
 async def useradd(client, message: Message, _):
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**Karena masalah privasi bot, Anda tidak dapat mengelola pengguna sudo saat menggunakan Database NekoXid.\n\n Harap isi MONGO_DB_URI Anda di vars untuk menggunakan fitur ini**"
+            "**Karena masalah privasi bot, Anda tidak dapat mengelola pengguna sudo saat menggunakan Database xid.\n\n Harap isi MONGO_DB_URI Anda di vars untuk menggunakan fitur ini**"
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
@@ -76,7 +76,7 @@ async def useradd(client, message: Message, _):
 async def userdel(client, message: Message, _):
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**Karena masalah privasi bot, Anda tidak dapat mengelola pengguna sudo saat menggunakan Database NekoXid.\n\n Harap isi MONGO_DB_URI Anda di vars untuk menggunakan fitur ini**"
+            "**Karena masalah privasi bot, Anda tidak dapat mengelola pengguna sudo saat menggunakan Database xid.\n\n Harap isi MONGO_DB_URI Anda di vars untuk menggunakan fitur ini**"
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
